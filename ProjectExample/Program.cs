de0f0c7376923d3794cde7ef6dfa6679b4871e3f
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-06-23 21:52:13 +0300

    Add project files.

---
+++ ProjectExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ProjectExample
+{
+    class Program
+    {
+        static void Task1()
+        {
+            //X^2 + 3*X – C
+            int X = Convert.ToInt32(Console.ReadLine());
+            int C = Convert.ToInt32(Console.ReadLine());
+
+            int result = X * X + 3 * X - C;
+
+            Console.WriteLine(result);
+        }
+
+        static void Task2()
+        {
+            Console.Write("Enter kilometers value:");
+            int kilometers = Convert.ToInt32(Console.ReadLine());
+
+            int meters = kilometers * 1000;
+            Console.WriteLine("Meters amount:" + meters);
+        }
+
+        static void Main(string[] args)
+        {
+
+            
+
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(a + b);
+            //Console.WriteLine(a - b);
+            //Console.WriteLine(a * b);
+            //Console.WriteLine(a / b);
+            //Console.WriteLine(a % b);
+        }
+    }
+}
